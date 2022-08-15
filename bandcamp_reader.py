ca434222a47540d597c90c30e8f9d75bcd9a958d
clumzy <george.pied@gmail.com>
2022-08-15 04:18:35 +0200

    Added a proper SQLite3 binding

--- bandcamp_reader.py
+++ bandcamp_reader.py
@@ -7,10 +7,15 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+import sqlite3
+
 class BandcampReader():
 
     def __init__(self) -> None:
         #AUTH PART
+        #ICI ON VERIFIE ET CREE SI IL MANQUE LE TOKEN QUI NOUS PERMET
+        #D'ACCEDER A L'API
+
         SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
         self.creds = None
         # The file token.json stores the user's access and refresh tokens, and is
@@ -31,39 +36,56 @@ class BandcampReader():
                 token.write(self.creds.to_json())
 
         #CONTENT PART
-        
+        #ICI ON VA SE CHARGER DE RAJOUTER LA PARTIE BDD QUI VA STOCKER LES SONS QUE
+        #NOUS AVONS DEJA SCANNES
+        self.db_links = sqlite3.connect('links.sqlite')
+        cursor_links = self.db_links.cursor()
+        query = """ SELECT name 
+                    FROM sqlite_master 
+                    WHERE type='table' 
+                    AND name='links';"""
+        if cursor_links.execute(query).fetchone() == None:
+            query = """ CREATE TABLE links(
+                        mail_id TEXT PRIMARY KEY UNIQUE,
+                        link TEXT NOT NULL,
+                        location TEXT);"""
+            cursor_links.execute(query)
+            print("Table links has been created !")
+        else:
+            print("Table links has been found. Connecting now !")
+        cursor_links.close()
+            
 
     def create_database(self):
         pass
 
     def get_content(self):
-        links_payload = []
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=self.creds)
         mails = service.users().messages().list(userId='me',q="label:bandcamp-releases subject:'new+release'", maxResults=500).execute()
+        cursor_links = self.db_links.cursor()
         messages = mails.get('messages')
         # messages is a list of dictionaries where each dictionary contains a message id.
         # iterate through all the messages
-        for msg in messages[:]:
-            print(msg['id'])
+        for msg in messages[:10]:
             # Get the message from its id
             txt = service.users().messages().get(userId='me', id=msg['id']).execute()
-            try:
-                # Get value of 'payload' from dictionary 'txt'
-                payload = txt['payload']
-                # The Body of the message is in Encrypted format. So, we have to decode it.
-                # Get the data and decode it with base 64 decoder.
-                parts = payload.get('parts')[0]
-                data = parts['body']['data']
-                data = data.replace("-","+").replace("_","/")
-                decoded_data = base64.b64decode(data)
-                # Now, the data obtained is in lxml. So, we will parse 
-                # it with BeautifulSoup library
-                soup = BeautifulSoup(decoded_data , "lxml")
-                links = str(soup.findAll("p")[0]).splitlines()
-                first_link = [link.split('?')[0] for link in links if link[:5]=="https"][0]
-                links_payload.append(first_link)
-            except:
-                print("Mail can't be decoded.")
-                pass
-        return links_payload
+            # Get value of 'payload' from dictionary 'txt'
+            payload = txt['payload']
+            # The Body of the message is in Encrypted format. So, we have to decode it.
+            # Get the data and decode it with base 64 decoder.
+            parts = payload.get('parts')[0]
+            data = parts['body']['data']
+            data = data.replace("-","+").replace("_","/")
+            decoded_data = base64.b64decode(data)
+            soup = BeautifulSoup(decoded_data , "lxml")
+            links = str(soup.findAll("p")[0]).splitlines()
+            msg_id = msg['id']
+            first_link = [link.split('?')[0] for link in links if link[:5]=="https"][0]
+            query = """ INSERT INTO links(mail_id, link)
+                        VALUES(?,?);"""
+            cursor_links = self.db_links.cursor()
+            cursor_links.execute(query, (msg_id, first_link))
+            self.db_links.commit()
+            cursor_links.close()
+        return None
